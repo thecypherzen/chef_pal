116a1ecbedb79016ef0b223d0449e12a1928a36a
William Inyam <sw.inyam@outlook.com>
2024-07-10 03:19:27 +0000

    style(App): manually arrange indentation

--- src/App.jsx
+++ src/App.jsx
@@ -9,35 +9,37 @@ import RecipeDetails from './components/RecipeDetails'
 import Instructions from './components/Instructions'
 import ErrorMessage from './components/ErrorMessage'
 
+
 export default function App() {
     const [data, setData] = useState([]);
     const [error, setError] = useState(null);
     const [itemId, setItemId] = useState(635675);
     const [isLoading, setLoading] = useState(true);
+    const [count, setCount] = useState(0);
 
     return (
 	<>
 	  <NavBar />
 	  <SearchBar setData={ setData } setLoading={ setLoading }
 		     setError={ setError }/>
-	  {
-	      error ?
-		  <div>
-			   <ErrorMessage error={ error } />
-		  </div> :
+	  { error ?
+	      <div>
+		    <ErrorMessage error={ error } />
+	      </div> :
 	      <>
-		  <FoodItems foodItems={ data } isLoading={ isLoading }
-                                       setItemId={ setItemId } error={ error }/>
-                            <DetailsContainer>
-                                  <InnerWrapper>
-                                        <RecipeDetails itemId={ itemId } isLoading={ isLoading } />
-                                      </InnerWrapper>
-                                      <InnerWrapper>
-                                            <Instructions />
-                                          </InnerWrapper>
-                                </DetailsContainer>
-
-	       </>
-	  }
+ 		    <FoodItems foodItems={ data } isLoading={ isLoading }
+				   setItemId={ setItemId }
+				   error={ error }/>
+  		    <DetailsContainer>
+                        <InnerWrapper>
+                              <RecipeDetails itemId={ itemId }
+						 isLoading={ isLoading } />
+   			</InnerWrapper>
+			<InnerWrapper>
+			      <Instructions />
+                        </InnerWrapper>
+                    </DetailsContainer>
+	      </>}
 	</>
-    )}
+    )
+}
